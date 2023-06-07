813eb40fcb964849b7f71661b53f6e708d5106f2
SantiagoRodriguezP <44353536+santiago197@users.noreply.github.com>
2023-06-06 21:55:47 -0500

    Card productos

--- app/src/main/java/com/zapateria/appzapateria/MainActivity.java
+++ app/src/main/java/com/zapateria/appzapateria/MainActivity.java
@@ -1,12 +1,15 @@
 package com.zapateria.appzapateria;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -52,8 +55,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.itemCarritoCompras) {
+            Toast.makeText(this, "Carrito de compras", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.itemCerrarSesion) {
+            Toast.makeText(this, "Cerrar sesión", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.itemNosotros) {
+            Toast.makeText(this, "Nosotros", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
